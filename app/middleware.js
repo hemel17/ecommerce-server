70d0f5535f6255d9f7a84c17adf5da08cc40326e
hemel17 <redewanmahmud@gmail.com>
2025-02-10 22:29:29 +0600

    cors options added

--- app/middleware.js
+++ app/middleware.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const middleware = [
   morgan("dev"),
-  cors(),
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  }),
+  cookieParser(),
   express.json(),
   express.urlencoded({ extended: true }),
 ];
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+require("dotenv").config();
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
+const dbUrl = process.env.DB_URL;
+const connectDB = require("./db");
 
-server.listen(port, () => {
-  console.log(`server is running at port : ${port}`);
-});
+// *database connection
+(async () => {
+  try {
+    await connectDB(dbUrl);
+    console.log("database is connected");
+    server.listen(port, () => {
+      console.log(`server is running at port : ${port}`);
+    });
+  } catch (error) {
+    console.log("failed to start the server", error);
+    process.exit(1);
+  }
+})();
